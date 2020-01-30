d1c153b6287a246769b1875f6a298f7b764a3885
José Sánchez-Gallego <gallegoj@uw.edu>
2020-01-29 21:35:12 -0800

    Removed some files

---
+++ tests/test_cherno.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2020-01-29
+# @Filename: test_cherno.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+
+def test_placeholder():
+
+    assert True
