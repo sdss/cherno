20caf7cb89e76d467e6e57583afa7bb7f5da19b3
José Sánchez-Gallego <gallegoj@uw.edu>
2021-01-31 13:38:44 -0800

    Revert to 3.8 and try using annotations from future

--- cherno/astrometry.py
+++ cherno/astrometry.py
@@ -6,6 +6,8 @@
 # @Filename: astrometry.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
+from __future__ import annotations
+
 import subprocess
 import time
 from os import PathLike
