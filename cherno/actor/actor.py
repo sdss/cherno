e69b2bf2e361c7b13f9477d73f17fb333a0ddfcb
José Sánchez-Gallego <gallegoj@uw.edu>
2022-08-21 21:10:53 -0400

    Add initial enabled axes

--- cherno/actor/actor.py
+++ cherno/actor/actor.py
@@ -83,7 +83,7 @@ class ChernoState:
         self.observatory = self.actor.observatory
         self.guide_loop = config["guide_loop"].copy()
         self.enabled_cameras = config["cameras"]["names"].copy()
-        self.enabled_axes = ["radec", "rot", "focus"]
+        self.enabled_axes = config["enabled_axes"].copy()
 
     def set_status(self, new_status: GuiderStatus, mode="override", report=True):
         """Sets the status and broadcasts it."""
